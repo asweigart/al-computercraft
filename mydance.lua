3d392ae6d5001553a42b24e3921bb369017cfe67
Al Sweigart <asweigart@gmail.com>
2016-07-28 15:20:03 -0700

    Added comments to dance programs.

--- mydance.lua
+++ mydance.lua
@@ -1,6 +1,14 @@
+-- Dance program
+-- By Al Sweigart
+-- al@inventwithpython.com
+-- Make the turtle dance!
+
 print('Hold Ctrl+T to stop dancing.')
 while true do
-  move = math.random(1, 4)
+  -- pick a random number
+  move = math.random(1, 2)
+
+  -- move based on random number
   if move == 1 then
     turtle.turnLeft()
   end
--- mydance2.lua
+++ mydance2.lua
@@ -1,15 +1,25 @@
+-- Dance program, version 2
+-- By Al Sweigart
+-- al@inventwithpython.com
+-- Make the turtle dance!
+
 isUp = false
 isBack = false
 
 print('Hold Ctrl+T to stop dancing.')
 while true do
+  -- pick random number
   move = math.random(1, 4)
+
+  -- turn moves:
   if move == 1 then
     turtle.turnLeft()
   end
   if move == 2 then
     turtle.turnRight()
   end
+
+  -- move forward/back:
   if move == 3 then
     if isBack == true then
       turtle.forward()
@@ -19,6 +29,8 @@ while true do
       isBack = true
     end
   end
+
+  -- move up/down:
   if move == 4 and isUp == true then
     turtle.down()
     isUp = false
