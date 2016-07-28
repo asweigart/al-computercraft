78e35482d6c6da7a626f186fe07fe8cb5365ec33
Al Sweigart <asweigart@gmail.com>
2016-07-28 13:21:51 -0700

    Added comments to maze runner program to get it into a publishable state.

--- mazerunner2.lua
+++ mazerunner2.lua
@@ -2,27 +2,27 @@ while true do
   if turtle.getFuelLevel() == 0 then
     return
   end
-
-  while true do
-    turtle.turnLeft()
-    if not turtle.detect() then
-      print('Moving left.')
-      break
-    end
+  
+  turtle.turnLeft()
+  if not turtle.detect() then
+    print('Moving left.')
+    turtle.forward()
+  else
     turtle.turnRight()
     if not turtle.detect() then
       print('Moving forward.')
-      break
+      turtle.forward()
+    else
+      turtle.turnRight()
+      if not turtle.detect() then
+        print('Moving right.')
+        turtle.forward()
+      else
+        turtle.turnRight()
+        print('Moving back.')
+        turtle.forward()
+      end
     end
-    turtle.turnRight()
-    if not turtle.detect() then
-      print('Moving right.')
-      break
-    end
-    turtle.turnRight()
-    print('Moving back.')
-    break
   end
-  
-  turtle.forward()
 end
+
