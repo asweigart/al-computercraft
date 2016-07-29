11772bc98d99d529cf8c5dac83bfc01887ada630
Al Sweigart <asweigart@gmail.com>
2016-07-29 11:54:41 -0700

    Added rest of brick factory programs.

---
+++ book/craftbricks.lua
@@ -0,0 +1,27 @@
+-- Craft Stone Bricks program
+-- By Al Sweigart
+-- al@inventwithpython.com
+-- Stone brick factory robot, 3 of 3
+
+print('Starting brick crafting program...')
+counter = 0
+while true do
+  turtle.select(1) -- load stone here
+  turtle.suckUp() -- get stone from furnace
+  if turtle.getItemCount() >= 4 then
+    -- make stone brick recipe
+    turtle.transferTo(2, 1)
+    turtle.transferTo(5, 1)
+    turtle.transferTo(6, 1)
+    turtle.select(16) -- craft to slot 16
+    turtle.craft() -- craft stone brick
+
+    counter = counter + 1
+    print('I have made ' .. counter .. ' bricks.')
+    turtle.drop() -- put stone brick into chest
+  else
+    -- wait before checking for stone again
+    print('Not enough stone yet. Sleeping...')
+    os.sleep(10)
+  end
+end
---
+++ book/furnacefueler.lua
@@ -0,0 +1,17 @@
+-- Furnace Fueler program
+-- By Al Sweigart
+-- al@inventwithpython.com
+-- Stone brick factory robot, 2 of 3
+
+while true do
+  if turtle.suckDown() then
+    print('No fuel in the chest.')
+  else
+    if turtle.drop() then
+      print('Furnace already full.')
+    else
+      print('Loaded some fuel.')
+    end
+  end
+  print('Sleeping...')
+  os.sleep(60)
