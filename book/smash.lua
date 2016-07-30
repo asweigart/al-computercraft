c5ff017301d16fbec166d2130d81cc8a1c6cacfe
Al Sweigart <asweigart@gmail.com>
2016-07-29 21:41:45 -0700

    Add cake smasher program

---
+++ book/smash.lua
@@ -0,0 +1,19 @@
+-- Cake Smash program
+-- By Al Sweigart
+-- al@inventwithpython.com
+-- Uses a piston to smash a cake!
+
+-- do a count down
+local countdown = 5
+while countdown > 0 do
+  print(countdown)
+  os.sleep(1)
+  countdown = countdown - 1
+end
+
+-- engage the piston
+redstone.setOutput('front', true)
+os.sleep(0.5)
+
+-- disengage the piston
+redstone.setOutput('front', false)
