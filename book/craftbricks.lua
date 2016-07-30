14fba9a0fcf13b747429f86dfb68bf4674451c03
Al Sweigart <asweigart@gmail.com>
2016-07-30 14:39:53 -0700

    Added option to craft bricks or just store stone.

--- book/craftbricks.lua
+++ book/craftbricks.lua
@@ -3,25 +3,33 @@
 -- al@inventwithpython.com
 -- Stone brick factory robot, 3 of 3
 
+ -- set to true to make bricks, false to leave as stone:
+local craftBricks = true
+
 print('Starting brick crafting program...')
 local counter = 0
 while true do
   turtle.select(1) -- load stone here
   turtle.suckUp() -- get stone from furnace
-  if turtle.getItemCount() >= 4 then
-    -- make stone brick recipe
-    turtle.transferTo(2, 1)
-    turtle.transferTo(5, 1)
-    turtle.transferTo(6, 1)
-    turtle.select(16) -- craft to slot 16
-    turtle.craft() -- craft stone brick
+  if craftBricks then
+    if turtle.getItemCount() >= 4 then
+      -- make stone brick recipe
+      turtle.transferTo(2, 1)
+      turtle.transferTo(5, 1)
+      turtle.transferTo(6, 1)
+      turtle.select(16) -- craft to slot 16
+      turtle.craft() -- craft stone brick
 
-    counter = counter + 4
-    print('I have made ' .. counter .. ' bricks.')
-    turtle.drop() -- put stone brick into chest
+      counter = counter + 4
+      print('I have made ' .. counter .. ' bricks.')
+      turtle.drop() -- put stone brick into chest
+    else
+      -- wait before checking for stone again
+      print('Not enough stone yet. Sleeping...')
+      os.sleep(10)
+    end
   else
-    -- wait before checking for stone again
-    print('Not enough stone yet. Sleeping...')
-    os.sleep(10)
+    -- put stone into chest without making bricks
+    turtle.drop() 
   end
 end
