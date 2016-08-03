fefade49d36ce428510d01e4325c6406aa5de075
Al Sweigart <asweigart@gmail.com>
2016-08-03 16:57:19 -0700

    Adding 3dprinter.lua file

---
+++ book/3dprinter.lua
@@ -0,0 +1,42 @@
+-- 3D Printer program
+-- By Al Sweigart
+-- turtleappstore.com/AlSweigart
+-- Prints out 
+
+print('Printing...')
+
+while true do
+  print('Collecting...')
+  for k, v in pairs({1,2,3,5,6,7,9,10,11}) do
+    turtle.select(v)
+    turtle.suck()
+  end
+
+  -- face the output barrel
+  turtle.turnLeft()
+  turtle.turnLeft()
+
+  while true do
+    turtle.select(16)
+    turtle.craft(64)
+    print('Crafted.')
+    itemData = turtle.getItemDetail(16)
+    if itemData == nil then break end
+    turtle.drop()
+    print('Deposited.')
+  end
+
+  -- face the resource barrel
+  turtle.turnLeft()
+  turtle.turnLeft()
+
+  -- handle any leftover resources
+  for k, v in pairs({1,2,3,5,6,7,9,10,11}) do
+    turtle.select(v)
+    turtle.drop() -- put it back into the resource barrel
+  end
+
+
+  print('Sleeping...')
+  os.sleep(60)
+end
