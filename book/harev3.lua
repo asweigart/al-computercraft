20af77671760abeb25d17b16409d26eaa7073b11
Al Sweigart <asweigart@gmail.com>
2016-08-08 15:52:22 -0700

    added building functions for walls, floors, rooms, and circle floors.

--- book/harev3.lua
+++ book/harev3.lua
@@ -3,7 +3,7 @@
 -- turtleappstore.com/users/AlSweigart
 -- Provides useful utility functions.
 
-hareVersion = "2"
+hareVersion = "3"
 
 -- fuelSpace() returns how much space
 -- for fuel there is left
@@ -113,13 +113,10 @@ end
 -- and columns of an area in front and
 -- to the right of the turtle, calling
 -- the provided sweepFunc at each point
--- sweepField() moves across the rows
--- and columns of an area in front and
--- to the right of the turtle, calling
--- the provided sweepFunc at each point
 function sweepField(rows, columns, sweepFunc)
   local turnRightNext = true
-  local columnStep, rowStep
+  local columnStep = 1
+  local rowStep = 1
   for columnStep = 1, columns do
     if sweepFunc ~= nil then
       sweepFunc(rowStep, columnStep, rows, columns)
@@ -178,11 +175,174 @@ end
 function countItems(name)
   local total = 0
   local slot
-  for slot=1,16 do
-    item = turtle.getItemDetail(slot)
+  for slot = 1,16 do
+    local item = turtle.getItemDetail(slot)
     if item ~= nil and item['name'] == name then
       total = total + item['count']
     end
   end
   return total
+end
+
+
+-- countInventory() returns the total
+-- number of items in the inventory
+function countInventory()
+  local total = 0
+  local slot
+  for slot = 1, 16 do
+    total = total + turtle.getItemCount(slot)
+  end
+  return total
+end
+
+
+-- buildRectangleFloor() builds a rectangle
+-- shaped floor out of the blocks in the
+-- inventory
+function buildRectangleFloor(length, width)
+  if countInventory() < length * width then
+    return false, 'Not enough blocks.'  -- not enough blocks
+  end
+  turtle.up()
+  sweepField(length, width, selectAndPlaceDown)
+end
+
+
+-- selectAndPlaceDown() selects a nonempty
+-- slot and places it under the turtle
+function selectAndPlaceDown()
+  local slot
+  for slot = 1, 16 do
+    if turtle.getItemCount(slot) > 0 then
+      turtle.select(slot)
+      turtle.placeDown()
+      break
+    end
+  end
+end
+
+
+-- buildWall() creates a wall stretching
+-- in front of the turtle
+function buildWall(length, height)
+  if countInventory() < length * height then
+    return false  -- not enough blocks
+  end
+
+  turtle.up()
+
+  local currentHeight, currentLength
+  local movingForward = true
+  for currentHeight = 1, height do
+    for currentLength = 1, length do
+      selectAndPlaceDown() -- place the block
+      if movingForward and currentLength ~= length then
+        turtle.forward()
+      elseif not movingForward and currentLength ~= length then
+        turtle.back()
+      end
+    end
+    if currentHeight ~= height then
+      turtle.up()
+    end
+    movingForward = not movingForward
+  end
+
+  -- done building wall, move to end position
+  local i
+  if movingForward then
+    -- turtle near the start position
+    for i = 1, length do
+      turtle.forward()
+    end
+  else
+    -- turtle near the end position
+    turtle.forward()
+  end
+
+  -- move down to the ground
+  for i = 1, height do
+    turtle.down()
+  end
+end
+
+
+-- 
+function buildRoom(length, width, height)
+  if countInventory() < ((length * height * height) - ((length - 1) * (height - 1) * height)) then
+    return false  -- not enough blocks
+  end
+
+  -- build the four walls
+  buildWall(length - 1, height)
+  turtle.turnRight()
+
+  buildWall(width - 1, height)
+  turtle.turnRight()
+  
+  buildWall(length - 1, height)
+  turtle.turnRight()
+  
+  buildWall(width - 1, height)
+  turtle.turnRight()
+end
+
+
+-- buildCircleFloor() builds a circle
+-- shaped floor out of the blocks in the
+-- inventory, centered on the turtle's
+-- starting position
+function buildCircleFloor(radius)
+  -- this is an overestimation of the blocks needed
+  local diameter = radius * 2 + 1
+  if countInventory() < (diameter * diameter)  then
+    return false, 'Not enough blocks.'  -- not enough blocks
+  end
+
+  -- move to start of sweep area
+  local i
+  turtle.up()
+  turtle.turnLeft()
+  for i = 1, math.floor(radius) do
+    turtle.forward()
+  end
+  turtle.turnRight()
+  for i = 1, math.floor(radius) do
+    turtle.back()
+  end
+
+  -- place blocks
+  sweepField(radius * 2 + 1, radius * 2 + 1, selectAndPlaceDownInCircle)
+
+  -- move back to center
+  for i = 1, math.floor(radius) do
+    turtle.forward()
+  end
+  turtle.turnRight()
+  for i = 1, math.floor(radius) do
+    turtle.forward()
+  end
+  turtle.turnLeft()
+end
+
+
+-- selectAndPlaceDownInCircle() selects 
+-- a nonempty slot and places it under 
+-- the turtle if it is within the circle's
+-- radius
+function selectAndPlaceDownInCircle(x, y, length, width)
+  local radius = math.floor(length / 2)
+  local centerx = math.ceil(length / 2)
+  local centery = math.ceil(width / 2)
+  
+  local xdistance = centerx - x
+  local ydistance = centery - y
+
+  if (math.sqrt(xdistance * xdistance + ydistance * ydistance) < radius) then
+    selectAndPlaceDown()
+      print(x, y, length, width, 'yes')
+  else
+      print(x, y, length, width, 'no')
+  end
 end
