2576d7722cb92e3c5c84d25e30113630b4bd9d23
Al Sweigart <asweigart@gmail.com>
2016-08-16 19:05:18 -0700

    Added print() calls so it's easier to trace the execution.

--- book/mydance.lua
+++ book/mydance.lua
@@ -7,9 +7,10 @@ print('Hold Ctrl+T to stop dancing.')
 while true do
   -- turn based on random number
   if math.random(1, 2) == 1 then
+  	print('Turn to the left!')
     turtle.turnLeft()
   else
+  	print('Turn to the right!')
     turtle.turnRight()
   end
 end
-
--- book/mydance2.lua
+++ book/mydance2.lua
@@ -9,39 +9,46 @@ isBack = false
 
 print('Hold Ctrl+T to stop dancing.')
 while true do
-  randomNum = math.random(1, 4)
+  randomNum = math.random(1, 5)
 
   if randomNum == 1 then
-    -- turn moves
-    if math.random(1, 2) == 1 then
-      turtle.turnLeft()
-    else
-      turtle.turnRight()
-    end
+    -- turn left
+    print('Turn to the left!')
+    turtle.turnLeft()
 
   elseif randomNum == 2 then
+    -- turn right
+    print('Turn to the right!')
+    turtle.turnRight()
+
+  elseif randomNum == 3 then
     -- forward/back moves
     if isBack then
+      print('Move forward!')
       turtle.forward()
       isBack = false
     else
+      print('Move back!')
       turtle.back()
       isBack = true
     end
 
-  elseif randomNum == 3 then
+  elseif randomNum == 4 then
     -- up/down moves
     if isUp then
+      print('Get up!')
       turtle.down()
       isUp = false
     else
+      print('Get down!')
       turtle.up()
       isUp = true
     end
 
-  elseif randomNum == 4 then
+  elseif randomNum == 5 then
     -- spin around
-    for i=1,4 do
+    print('Spin!')
+    for i = 1, 4 do
       turtle.turnLeft()
     end
   end
